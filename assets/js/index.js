30ffdfd1b433e2b2c8cd677da45069bd0a91b992
YusufByn <yusuf.buyukaydin33@gmail.com>
2025-07-21 16:49:21 +0200

    second-commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -54,3 +54,15 @@ console.log(nousTxt);
 let wordVerifier = nousTxt.includes("nous");
 
 console.log(wordVerifier);
+
+// une fonction qui me génère un chiffre aléatoirement entre 1 et 10.
+
+let C =  Math.round(Math.random() * 9 + 1);
+// math.round sert à arrondir les nombres, si tu fais juste math.random * 9 + 1
+// on te propose des chiffres du type 5.5662777.
+// je rajoute un + 1 dans la ligne de code sinon y'a la possibilitée d'avoir le chiffre 0
+// et je mets 9 car sinon ca peut aller jusque 11 et non 10max comme demandé dans la consigne
+// ca va jusque 11 car le math.round arrondi au nombre entier le plus proche, donc si je mets * 10
+// je peux avoir le résultat 10.32 par ex, donc ca pourrai arrondir à 11
+
+console.log(C);
